ce5e72b31b46acce133ab113dbeead73fce8a8f2
Mark Robinson <mark.robinson@mls.uzh.ch>
2024-04-26 12:42:50 +0200

    couple extra py examples

---
+++ mark/for-if.py
@@ -0,0 +1,12 @@
+
+x = range(0,10)
+
+print(x)
+
+print(x[0])
+print(x[9])
+
+for i in range(1,10):
+  if i > 5:
+    print(i)
+
---
+++ mark/mean.py
@@ -0,0 +1,9 @@
+
+import numpy as np
+
+x = [1, 2, 3, 4]
+np_x = np.array(x)
+
+print(np_x)
+print(np.mean(np_x))
+
